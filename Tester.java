3d42e41acba0a52d6fb55f410e9900715b94454b
Ladyerny <163018376+Ladyerny@users.noreply.github.com>
2024-03-11 16:32:37 +0100

    Create Tester.java

---
+++ Tester.java
@@ -0,0 +1,13 @@
+package master_Mind;
+
+public class Tester {
+
+	public static void main(String[] args) {
+		//Combinacion c1 = new Combinacion();
+		//c1.gereraCSecreta(2, 8);
+		Mastermind juego = new Mastermind();
+		juego.iniciarJuego();
+
+	}
+
+}
